83e1beef323face458febbaca46073a3b6ba5993
Udhbhav <21889udbhav@gmail.com>
2023-06-17 14:22:39 +0530

    Styled tweet form

--- src/pages/components/Tweet.tsx
+++ src/pages/components/Tweet.tsx
@@ -3,30 +3,6 @@ import Image from 'next/image';
 import React, { use } from 'react'
 import { prisma } from '~/server/db';
 import TweetForm from './TweetForm';
-type User = {
-    name: string | null;
-    email: string | null;
-    image: string | null;
-    id: string;
-};
-
-// const getUser = async (): Promise<User | null> => {
-
-//     const userId = useSession().data?.user?.id; // Assuming this is a valid function call
-
-//     try {
-//         const user = await prisma.user.findUnique({
-//             where: {
-//                 id: userId,
-//             },
-//         });
-
-//         return user;
-//     } catch (err) {
-//         console.log(err);
-//         return null;
-//     }
-// };
 
 function Tweet() {
     const userImage = useSession().data?.user?.image;
@@ -35,14 +11,13 @@ function Tweet() {
     }
     return (
         <>
-            <div className='flex  '>
+            <div className='flex w-2/3  border-r-2 border-b-2 border-slate-800'>
                 <div className='p-4 '>
                     <Image alt='image' src={userImage} width={50} height={50} quality={100} className='rounded-full' />
                 </div>
                 <div className='w-full pt-2'>
                     <TweetForm />
                     <div>
-                        
                     </div>
                 </div>
 
--- src/pages/components/TweetForm.tsx
+++ src/pages/components/TweetForm.tsx
@@ -1,25 +1,28 @@
 import React, { use, useEffect } from 'react'
 
 function TweetForm() {
-  useEffect(() => {
-    const textarea = document.querySelector('textarea');
-    textarea?.addEventListener('input', autoResize, false);
-    function autoResize(this: any) {
-        this.style.height = 'auto';
-        this.style.height = this.scrollHeight + 'px';
-    }
+    useEffect(() => {
+        const textarea = document.querySelector('textarea');
+        textarea?.addEventListener('input', autoResize, false);
+        function autoResize(this: any) {
+            this.style.height = 'auto';
+            this.style.height = this.scrollHeight + 'px';
+        }
     }, [])
 
-  return (
-    <>
-    <form className='px-2 w-7/12 '>
-        <textarea 
-        placeholder="What's happening?" 
-        className='w-full outline-none h-full bg-transparent resize-none'
-        style={{height: 50}} />
-    </form>
-    </>
-  )
+    return (
+        <>
+            <form className='px-2 w- pt-4 '>
+                <textarea
+                    placeholder="What's happening?!"
+                    className='w-11/12 outline-none h-full bg-transparent resize-none border-b-2  border-slate-800 placeholder:text-xl'
+                    style={{ height: 50 }} />
+                <div className=' translate-x-full border-'>
+                    <button className='bg-blue-500 rounded-full px-4  -translate-x-full  py-2  mt-2 text-white font-bold mb-2'>Tweet</button>
+                </div>
+            </form>
+        </>
+    )
 }
 
 export default TweetForm
