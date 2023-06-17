404ef72792883bbdda8c1834b77ceb5bbf536bda
Udhbhav <21889udbhav@gmail.com>
2023-06-17 13:52:55 +0530

    Styled and add photo of user

--- src/pages/components/Tweet.tsx
+++ src/pages/components/Tweet.tsx
@@ -2,6 +2,7 @@ import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 import React, { use } from 'react'
 import { prisma } from '~/server/db';
+import TweetForm from './TweetForm';
 type User = {
     name: string | null;
     email: string | null;
@@ -28,14 +29,25 @@ type User = {
 // };
 
 function Tweet() {
-    const userImage =  useSession().data?.user?.image;
+    const userImage = useSession().data?.user?.image;
     if (!userImage) {
         return <>Loading...</>;
     }
     return (
-        <div className='p-4'>
-            <Image alt='image' src={userImage} width={50} height={50} quality={100} className='rounded-full'/>
-        </div>
+        <>
+            <div className='flex  '>
+                <div className='p-4 '>
+                    <Image alt='image' src={userImage} width={50} height={50} quality={100} className='rounded-full' />
+                </div>
+                <div className='w-full pt-2'>
+                    <TweetForm />
+                    <div>
+                        
+                    </div>
+                </div>
+
+            </div>
+        </>
     )
 }
 
